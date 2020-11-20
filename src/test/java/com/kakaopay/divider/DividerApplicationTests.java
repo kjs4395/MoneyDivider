bead36e6c0acfe81ef468c4edfb6de18ceba6c60
kjs4395 <kjs4395@gmail.com>
2020-11-20 19:09:48 +0900

    project init

---
+++ src/test/java/com/kakaopay/divider/DividerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaopay.divider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
